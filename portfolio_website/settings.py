cb0a89246c7bc5957ea54f307695e393a65233ab
sakalaharshith <sakala.harshith@gmail.com>
2025-01-07 13:54:11 +0530

    'madechangestoimagepaths'

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
