7ec8db1c79c8181dbb348977f8c230495c4b79e0
sakalaharshith <sakala.harshith@gmail.com>
2025-01-08 17:38:10 +0530

    'all_images_are_added'

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -132,8 +132,8 @@ AWS_STORAGE_BUCKET_NAME = 'harshith-portfolio-storage'
 AWS_S3_REGION_NAME = 'eu-west-2'  # or your chosen region
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') 
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-print(f"AWS_ACCESS_KEY_ID: {AWS_ACCESS_KEY_ID}")
-print(f"AWS_SECRET_ACCESS_KEY: {AWS_SECRET_ACCESS_KEY}")
+#print(f"AWS_ACCESS_KEY_ID: {AWS_ACCESS_KEY_ID}")
+#print(f"AWS_SECRET_ACCESS_KEY: {AWS_SECRET_ACCESS_KEY}")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
