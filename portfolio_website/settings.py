b4e5e9a8db802514bad3812999ecb64327b453f2
sakalaharshith <sakala.harshith@gmail.com>
2025-01-08 11:53:40 +0530

    'added_awsaccesskey'

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os 
 from storages.backends.s3boto3 import S3Boto3Storage
-
+from dotenv import load_dotenv
+# Loading environment variables to access the application token
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,8 +141,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_STORAGE_BUCKET_NAME = 'my-django-portfolio-bucket'
 AWS_S3_REGION_NAME = 'eu-west-2'  # or your chosen region
-AWS_ACCESS_KEY_ID = 'AKIA4ZPZVRW5NDHZD5OM' 
-AWS_SECRET_ACCESS_KEY = 'NNSSALDq/6yeXGrRdpEIvZvU7AWreapOzf6KgCrQ'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') 
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 
 # If you want to serve both static and media from S3:
